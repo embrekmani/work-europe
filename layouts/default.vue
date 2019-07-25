8021e7ec5c0934e4c852b062d3ff8857d58a0c19
Embrek <embrekmani@gmail.com>
2019-07-25 13:36:21 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html, body {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.el-container.outer {
+  max-width: 1216px;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-container class="outer">
+    <el-aside width="400px">
+      <h1>Find your place in Europe.</h1>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida mi sem, at aliquet neque interdum nec. Duis convallis semper cursus. Vestibulum efficitur lorem erat. Cras sed dui vel mi rhoncus vestibulum sed eget urna. Nullam sapien metus, rhoncus in nunc quis, imperdiet sagittis risus.</p>
+    </el-aside>
+
+    <el-main>    
+      <el-carousel height="50vh" trigger="click">
+        <el-carousel-item v-for="(img, i) in images" :key="i">
+          <el-image :src="getImageUrl(img)" fit="contain"/>
+        </el-carousel-item>
+      </el-carousel>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      images: ['benjamin-davies-Oja2ty_9ZLM-unsplash.jpg', 'chris-karidis-nnzkZNYWHaU-unsplash.jpg', 'stefan-widua-iPOZf3tQfHA-unsplash.jpg']
+    }
+  },
+  methods: {
+    getImageUrl(url) {
+      return require('../static/' + url)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.el-aside {
+  padding: 1rem;
+  h1 {
+    margin: 0;
+    font-weight: 300;
+    font-size: 2.5rem;
+    color: #303133;
+  }
+  p {
+    color: #606266;
+  }
+}
+
+.el-carousel {
+  width: 100%;
+  border-radius: .25rem;
+}
+</style>
